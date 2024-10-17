ace380b1b8779c0baa43513eddfe4209f4a63ab5
davidvvvzhang <2801442102@qq.com>
2024-10-17 09:17:57 +0800

    Update server.cpp
    解决了windows和Unix-like平台的兼容问题

--- server.cpp
+++ server.cpp
@@ -2,15 +2,30 @@
 
 #include <iostream>
 #include <cstring>
-#include <sys/socket.h> // 包含创建和操作套接字的函数，如socket()、bind()、listen()等
-#include <netinet/in.h> // 定义了地址族AF_INET和端口的结构体sockaddr_in
-#include <arpa/inet.h> // 用于处理地址转换的函数，如inet_pton()
-#include <unistd.h> // 包含系统调用，如close()，用于关闭套接字
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #pragma comment(lib, "ws2_32.lib")
+    #define close closesocket
+#else
+    #include <sys/socket.h> // 包含创建和操作套接字的函数，如socket()、bind()、listen()等
+    #include <netinet/in.h> // 定义了地址族AF_INET和端口的结构体sockaddr_in
+    #include <arpa/inet.h> // 用于处理地址转换的函数，如inet_pton()
+    #include <unistd.h> // 包含系统调用，如close()，用于关闭套接字
+#endif
 
 #define PORT 8888 // 定义端口号8888，服务器将使用这个端口号监听客户端连接
 #define BUFFER_SIZE 1024 // 定义缓冲区大小为1024字节，用于存储接收到的数据
 
 int main() {
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed" << std::endl;
+        return -1;
+    }
+#endif
+    
     // 创建服务器端的套接字
     int server_fd, new_socket; // server_fd是服务器的套接字描述符，用于创建套接字，new_socket用于接收客户端的连接
     struct sockaddr_in address; // 定义了一个sockaddr_in结构体，用于存储服务器的IP地址和端口信息
@@ -73,5 +88,10 @@ int main() {
     // 关闭客户端和服务器的套接字，释放资源
     close(new_socket);
     close(server_fd);
+    
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    
     return 0;
 }
